56b99291245d1da7a1e25a5fe7cc777336f37707
Sansan2312 <fklasich@gmail.com>
2025-05-08 00:20:27 +0200

    Add files via upload

---
+++ Scripts/Pieces/Queen.cs
@@ -0,0 +1,75 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Queen : ChessPiecesBase
+{
+    public override void Init(bool isWhite, Vector2Int position)
+    {
+        base.Init(isWhite, position);
+        this.pieceType = PieceType.Queen;
+    }
+    public override List<Vector2Int> GetLegalMoves()
+    {
+        var currentMemory = ChessManager.Instance.Memory;
+        List<Vector2Int> legalMoves = new List<Vector2Int>();
+
+        // Directions for rook-like movement (left, right, up, down)
+        Vector2Int[] rookDirections = new Vector2Int[]
+        {
+            new Vector2Int(1, 0),  // Right
+            new Vector2Int(-1, 0), // Left
+            new Vector2Int(0, 1),  // Up
+            new Vector2Int(0, -1)  // Down
+        };
+
+        // Directions for bishop-like movement (top-right, top-left, bottom-right, bottom-left)
+        Vector2Int[] bishopDirections = new Vector2Int[]
+        {
+            new Vector2Int(1, 1),  // Top-right diagonal
+            new Vector2Int(-1, 1), // Top-left diagonal
+            new Vector2Int(1, -1), // Bottom-right diagonal
+            new Vector2Int(-1, -1) // Bottom-left diagonal
+        };
+
+        // Combine both rook and bishop movements
+        Vector2Int[][] allDirections = new Vector2Int[][] { rookDirections, bishopDirections };
+
+        foreach (var directions in allDirections)
+        {
+            foreach (var direction in directions)
+            {
+                int count = 1;
+
+                while (true)
+                {
+                    Vector2Int newPosition = position + direction * count;
+
+                    if (IsWithinBounds(newPosition))
+                    {
+                        if (!IsOccupied(currentMemory, newPosition))
+                        {
+                            legalMoves.Add(newPosition);
+                        }
+                        else if (IsCapturable(currentMemory, newPosition, isWhite))
+                        {
+                            legalMoves.Add(newPosition);
+                            break;
+                        }
+                        else
+                        {
+                            break;
+                        }
+                    }
+                    else
+                    {
+                        break;
+                    }
+
+                    count++;
+                }
+            }
+        }
+
+        return legalMoves;
+    }
+}
---
+++ Scripts/Utils/JSON_Coder.cs
@@ -0,0 +1,44 @@
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+[System.Serializable]
+public class ChessMove
+{
+    public string pieceType;
+    public string from;
+    public string to;
+    public bool isWhite;
+}
+
+[System.Serializable]
+public class MoveHistory
+{
+    public List<ChessMove> moves = new List<ChessMove>();
+}
+
+
+public class MoveLogger : MonoBehaviour
+{
+    public static MoveHistory moveHistory = new MoveHistory();
+
+    public static void LogMove(string pieceType, string from, string to, bool isWhite)
+    {
+        ChessMove move = new ChessMove
+        {
+            pieceType = pieceType,
+            from = from,
+            to = to,
+            isWhite = isWhite
+        };
+
+        moveHistory.moves.Add(move);
+    }
+
+    public static void SaveToJson(string filename = "chess_moves.json")
+    {
+        string json = JsonUtility.ToJson(moveHistory, true);
+        string path = Path.Combine(Application.persistentDataPath, filename);
+        File.WriteAllText(path, json);
+        Debug.Log("Saved move history to: " + path);
+    }
+}
